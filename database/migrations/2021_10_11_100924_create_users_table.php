2ba9e0c04b16d68228f56fffdbef887ea5aed7ef
yoshihiroyoneda <yo4a2985fyoshihiro@icloud.com>
2021-10-11 19:22:14 +0900

    commit

--- database/migrations/2021_10_11_100924_create_users_table.php
+++ database/migrations/2021_10_11_100924_create_users_table.php
@@ -14,11 +14,11 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->bigIncrements('id')->unique();
+            $table->string('name',40);
+            $table->string('email',255)->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password',30);
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2021_10_11_100955_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id')->unique();
+            $table->string('title',255);
+            $table->string('body',2000);
+            $table->string('publish',30);
+            $table->timestamps('created_at');
+            $table->timestamps('updated_at');
+            $table->timestamps('deleted_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
